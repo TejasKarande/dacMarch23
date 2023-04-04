a013cee1b814963172a4c396e3266a15613d5ab8
TejasKarande <128144813+TejasKarande@users.noreply.github.com>
2023-04-04 21:56:31 +0530

    Add files via upload

---
+++ Assignments/Pattern/PatternHollowInverted_Half.java
@@ -0,0 +1,16 @@
+class PatternHollowInverted_Half{
+
+    public static void main(String[] args) {
+
+        for (int i = 6; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                if (j == i || j == 1 || i == 6) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
